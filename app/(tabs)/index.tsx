20377ea6def752a107c5d4d11be6a1ed1a568e3e
hyunho4532 <kyounguk192@gmail.com>
2024-11-03 19:27:18 +0900

    chore . emotion 라이브러리 add .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
+import { Title } from '@/ui-kit/shared/Title';
 import { View } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
 
 export default function HomeScreen() {
   return (
     <View>
-      <ThemedText>테스트입니다.</ThemedText>
+      <Title>테스트입니다.</Title>
     </View>
   );
 }
---
+++ ui-kit/shared/Title.tsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/native'
+
+export const Title = styled.Text`
+    font-size: 22px;
+`
