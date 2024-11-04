a7368eb10ca51ddc6697951c03681b43cd733284
hyunho4532 <kyounguk192@gmail.com>
2024-11-04 21:30:21 +0900

    feat . 메인 페이지 구현 중

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View>
-      <Title fontSize='24' align='center'>테스트입니다.</Title>
+      <Title fontSize='24' align='left' paddingTop='40' paddingLeft='16'>오늘의 날씨를 알려드립니다.</Title>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,21 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
-import * as Font from 'expo-font';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import SplashScreen from './(splash)';
+import * as Font from 'expo-font';
+import * as Splash from 'expo-splash-screen';
+import SplashScreen from './splash';
 
+Splash.preventAutoHideAsync();
 
 export default function RootLayout() {
 
   const [appIsReady, setAppIsReady] = useState(false);
 
   const colorScheme = useColorScheme();
-
-  Font.useFonts({
-    Pretendard: require('../assets/fonts/Pretendard-Bold.ttf')
+  
+  Font.loadAsync({
+    'Pretendard': require('../assets/fonts/Pretendard-Bold.ttf')
   })
 
   useEffect(() => {
@@ -37,7 +39,7 @@ export default function RootLayout() {
       <SplashScreen appIsReady={appIsReady} />
     )
   }
-
+  
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
