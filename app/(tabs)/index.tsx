122cb2e10b2268db059ed74ae8bab293f115c017
hyunho4532 <kyounguk192@gmail.com>
2024-11-05 23:28:48 +0900

    feat . mobileImage, webImage 컴포넌트 분리

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Title } from '@/ui-kit/shared/Title';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { useFetchWeather } from './api/fetch';
 import { Container } from '@/ui-kit/shared/Container';
 import { calculateCelsiusFromKelvin } from '@/ui-kit/calcul';
-import { Image } from '@/ui-kit/shared/Image';
+import { MobileImage, WebImage } from '@/ui-kit/shared/Image';
 
 /** 가장 메인이자 홈 스크린 */
 export default function HomeScreen() {
@@ -17,25 +17,26 @@ export default function HomeScreen() {
   if (isPending) return <Title fontSize='14'>Loading...</Title>
 
   return (
-    <>
-      { data && 
-        <View>
-          <Title fontSize='24' align='left' paddingTop='40' paddingLeft='16'>
-            오늘의 날씨를 알려드립니다.
-          </Title>
+    <View>
+      <Title fontSize='24' align='left' paddingTop='40' paddingLeft='16'>
+        오늘의 날씨를 알려드립니다.
+      </Title>
 
-          <Container width='220' height='160' auto='auto' color='white' marginTop='24'>
-            <Image width={100} height={100} src={`https://openweathermap.org/img/wn/03n.png`} />
-            <Title fontSize='18' paddingTop='8' align='center'>
-              온도: {calculateCelsiusFromKelvin(data.main.temp)}
-            </Title>
-          </Container>
+      <Container width='220' height='160' auto='auto' color='white' marginTop='24'>
+        { Platform.OS === 'web' ? 
+          <WebImage width={100} height={100} src={`https://openweathermap.org/img/wn/03n.png`} /> : 
+          <MobileImage width={100} height={100} source={{
+            uri: `https://openweathermap.org/img/wn/03n.png`
+          }} />
+        }
+        <Title fontSize='18' paddingTop='8' align='center'>
+          온도: {calculateCelsiusFromKelvin(data.main.temp)}
+        </Title>
+      </Container>
 
-          <Title fontSize='18' paddingTop='24' paddingLeft='16'>
-            🌫 현재 바람: {data.wind.speed}ms
-          </Title>
-        </View> 
-      }
-    </>
+      <Title fontSize='18' paddingTop='24' paddingLeft='16'>
+        🌫 현재 바람: {data.wind.speed}ms
+      </Title>
+    </View> 
   );
 }
--- ui-kit/shared/Image.tsx
+++ ui-kit/shared/Image.tsx
@@ -1,9 +1,18 @@
-import styled from "@emotion/styled"
+import webStyled from "@emotion/styled"
+import mobileStyled from "@emotion/native"
 
+export const MobileImage = mobileStyled.Image<{
+    width: number,
+    height: number
+}>`
+    width: ${(props) => props.width}px;
+    height: ${(props) => props.height}px;
+    margin: 0 auto;
+`
 
-export const Image = styled.img<{
-    width?: number,
-    height?: number
+export const WebImage = webStyled.img<{
+    width: number,
+    height: number
 }>`
     width: ${(props) => props.width}px;
     height: ${(props) => props.height}px;
